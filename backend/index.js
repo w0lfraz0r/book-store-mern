8e50e4b204000ac9ce7aab7c9508400f2e54ade9
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2023-09-13 17:39:07 +0530

    added post and book  modal

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/books", async (req, res) => {
+    const {title, author, publishYear} = req.body;
+    try {
+        if (!title || !author || !publishYear){
+            res.status(400).send({message: 'Invalid Input'});
+        } else {
+            const newBook = {title, author, publishYear};
+            const book = await Book.create(newBook);
+            res.status(201).send(book);
+        }
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
 });
 
 mongoose
