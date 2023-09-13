2a04e18c768bdfbfbf7de2daa3c6518864992ad4
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2023-09-13 19:06:48 +0530

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBooks from "./pages/ShowBooks";
 
 function App() {
   return (
-    <div className='bg-red-400 text-white'>App</div>
-  )
+    <Routes>
+      <Route path='/' element={Home} />
+      <Route path='/books/create' element={CreateBooks} />
+      <Route path='/books/delete/:id' element={DeleteBook} />
+      <Route path='/books/edit/:id' element={EditBook} />
+      <Route path='/books/details/:id' element={ShowBooks} />
+      {/* <div className="bg-red-400 text-white">App</div> */}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
