0f1318a36ef6ea4668ffc615af63003306085e93
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2023-09-13 16:56:29 +0530

    connected to monose db

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://w0lfraz0r:w0lfraz0r@cluster0.abvrts7.mongodb.net/?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
